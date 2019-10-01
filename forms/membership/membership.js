ed5dde78cd56fe754a2382fa1fe2b95ece3a9f70
Connor <cmg58665@gmail.com>
2019-10-01 16:24:10 -0500

    2

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,17 @@
 /*
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+btnSearchName.onclick=function(){
+  if (members.includes(inptFirstName.value)) {
+    alertMember.value = "You are a member.";
+    inptFirstName.value = '';
+    
+  } else {
+      alertMember.value = "You are not a member yet, but we will add you to our system.";
+      members.push(inptFirstName.value);
+      inptFirstName.value = '';
+      
+    }
+  
+}
+/*
